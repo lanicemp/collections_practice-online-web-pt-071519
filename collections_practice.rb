4eff77590fb37b13f8c26d26b6cba2850591e152
LaNice Powell <lanicemp@aol.com>
2019-07-25 09:23:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,7 +14,7 @@ def sort_array_char_count(list)
 
 end 
 
-def swap_elements(array, index, destination_index)
+def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   binding.pry  
 end 
