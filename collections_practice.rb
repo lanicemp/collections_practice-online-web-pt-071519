f478129405405fbb39cb1ee54ebad1a2c9e917e5
LaNice Powell <lanicemp@aol.com>
2019-07-25 01:40:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,12 @@
+def  sort_array_asc(list)
+  list.sort
+end 
 
+def sort_array_desc(list)
+  list.sort {|x, y| y <=> x}
+end 
+
+def sort_array_char_count(list)
+  list.sort {|x,y|}
+  binding.pry
+end 
